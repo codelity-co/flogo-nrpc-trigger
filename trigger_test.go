9b5e88c13c3995e529bfbc6235c93ac1aefe9d5f
Reid Lai <reidlai@gmail.com>
2020-11-24 21:53:03 +0800

    update unit test

--- trigger_test.go
+++ trigger_test.go
@@ -1,11 +1,11 @@
 package nrpc
 
 import (
-	"github.com/project-flogo/core/data/metadata"
 	"encoding/json"
 	"testing"
 
 	"github.com/project-flogo/core/action"
+	"github.com/project-flogo/core/data/metadata"
 	"github.com/project-flogo/core/support"
 	"github.com/project-flogo/core/support/log"
 	"github.com/project-flogo/core/support/test"
@@ -17,25 +17,6 @@ import (
 	"github.com/nats-io/gnatsd/server"
 	natsserver "github.com/nats-io/nats-server/test"
 )
-
-const testConfig string = `{
-	"id": "flogo-timer",
-	"ref": "github.com/project-flogo/contrib/trigger/kafka",
-	"settings": {
-	  "brokerUrls": "localhost:9092"
-	},
-	"handlers": [
-	  {
-			"action":{
-				"id":"dummy"
-			},
-			"settings": {
-		  	"topic": "syslog"
-			}
-	  }
-	]
-	
-}`
 
 type TriggerTestSuite struct {
 	suite.Suite
@@ -66,37 +47,40 @@ func (suite *TriggerTestSuite) SetupTest() {
 }
 
 func (suite *TriggerTestSuite) TestFactoryNew() {
+	t := suite.T()
 
 	ref := support.GetRef(&Trigger{})
 	f := trigger.GetFactory(ref)
-	assert.NotNil(suite.T(), f, "Should return factory instance")
+	assert.NotNil(t, f, "Should return factory instance")
 
 }
 
 func (suite *TriggerTestSuite) TestFactoryMetadata() {
+	t := suite.T()
 
 	ref := support.GetRef(&Trigger{})
 	f := trigger.GetFactory(ref)
-	assert.NotNil(suite.T(), f, "Should return factory instance")
+	assert.NotNil(t, f, "Should return factory instance")
 
 	m := f.Metadata()
-	assert.NotNil(suite.T(), m, "Factory should return its metadata")
+	assert.NotNil(t, m, "Factory should return its metadata")
 	
 }
 
 func (suite *TriggerTestSuite) TestTriggerInitialize() {
+	t := suite.T()
 
-	s := RunServerOnPort(4222)
+	s := RunServerWithOptions()
 	defer s.Shutdown()
 
 	ref := support.GetRef(&Trigger{})
 	f := trigger.GetFactory(ref)
-	assert.NotNil(suite.T(), f, "Should return factory instance")
+	assert.NotNil(t, f, "Should return factory instance")
 
 	// Test trigger configuration
 	config := &trigger.Config{}
 	err := json.Unmarshal([]byte(suite.testConfig), config)
-	assert.Nil(suite.T(), err, "Invalid trigger config")
+	assert.Nil(t, err, "Invalid trigger config")
 
 	// Create dummy action
 	actions := map[string]action.Action{"dummy": test.NewDummyAction(func() {
@@ -105,63 +89,70 @@ func (suite *TriggerTestSuite) TestTriggerInitialize() {
 
 	// Test trigger instance
 	trg, err := test.InitTrigger(f, config, actions)
-	assert.Nil(suite.T(), err, "InitTrigger return error")
-	assert.NotNil(suite.T(), trg, "Should return trigger instance")
+	assert.Nil(t, err, "InitTrigger return error")
+	assert.NotNil(t, trg, "Should return trigger instance")
 
 }
 
 
 func (suite *TriggerTestSuite) TestGetNatsConnAuthOpts() {
+	t := suite.T()
+
 	config := &trigger.Config{}
 	err := json.Unmarshal([]byte(suite.testConfig), config)
-	assert.Nil(suite.T(), err, "Invalid trigger config")
+	assert.Nil(t, err, "Invalid trigger config")
 
 	s := &Settings{}
 	err = metadata.MapToStruct(config.Settings, s, true)
-	assert.Nil(suite.T(), err, "MapToStruct error when converting json to Settings")
+	assert.Nil(t, err, "MapToStruct error when converting json to Settings")
 
 	_, err = getNatsConnAuthOpts(s)
-	assert.Nil(suite.T(), err, "getNatsConnAuthOpts error")
+	assert.Nil(t, err, "getNatsConnAuthOpts error")
 }
 
 func (suite *TriggerTestSuite) TestGetNatsConnReconnectOpts() {
-	
+	t := suite.T()
+
 	config := &trigger.Config{}
 	err := json.Unmarshal([]byte(suite.testConfig), config)
 	assert.Nil(suite.T(), err, "Invalid trigger config")
 
 	s := &Settings{}
 	err = metadata.MapToStruct(config.Settings, s, true)
-	assert.Nil(suite.T(), err, "MapToStruct error when converting json to Settings")
+	assert.Nil(t, err, "MapToStruct error when converting json to Settings")
 
 	_, err = getNatsConnAuthOpts(s)
-	assert.Nil(suite.T(), err, "getNatsConnAuthOpts error")
+	assert.Nil(t, err, "getNatsConnAuthOpts error")
 }
 
 func (suite *TriggerTestSuite) TestGetNatsConnSslConfigOpts() {
+	t := suite.T()
+
 	config := &trigger.Config{}
 	err := json.Unmarshal([]byte(suite.testConfig), config)
-	assert.Nil(suite.T(), err, "Invalid trigger config")
+	assert.Nil(t, err, "Invalid trigger config")
 
 	s := &Settings{}
 	err = metadata.MapToStruct(config.Settings, s, true)
-	assert.Nil(suite.T(), err, "MapToStruct error when converting json to Settings")
+	assert.Nil(t, err, "MapToStruct error when converting json to Settings")
 
 	_, err = getNatsConnSslConfigOpts(s)
-	assert.Nil(suite.T(), err, "getNatsConnSslConfigOpts error")
+	assert.Nil(t, err, "getNatsConnSslConfigOpts error")
 }
 
 func (suite *TriggerTestSuite) TestHandlerGetConnection() {
-	s := RunServerOnPort(4222)
+	t := suite.T()
+
+	s := RunServerWithOptions()
 	defer s.Shutdown()
 
 	config := &trigger.Config{}
 	err := json.Unmarshal([]byte(suite.testConfig), config)
-	assert.Nil(suite.T(), err, "Invalid trigger config")
+	assert.Nil(t, err, "Invalid trigger config")
 	
 	triggerSettings := &Settings{}
 	err = metadata.MapToStruct(config.Settings, triggerSettings, true)
-	assert.Nil(suite.T(), err, "MapToStruct error when converting json to Settings")
+	assert.Nil(t, err, "MapToStruct error when converting json to Settings")
 
 	h := &Handler{
 		logger: log.RootLogger(),
@@ -169,22 +160,24 @@ func (suite *TriggerTestSuite) TestHandlerGetConnection() {
 	}
 
 	err = h.getConnection()
-	assert.Nil(suite.T(), err, "getConnection error")
-	assert.NotNil(suite.T(), h.natsConn, "Cannot get NATS connection")
+	assert.Nil(t, err, "getConnection error")
+	assert.NotNil(t, h.natsConn, "Cannot get NATS connection")
 }
 
 func (suite *TriggerTestSuite) TestResolveObject() {
+	t := suite.T()
+
 	_, err := resolveObject(map[string]interface{}{
 		"test1": "=$property[TEST]",
 	})
-	assert.NotNil(suite.T(), err, "resolveObject error")
-	assert.Equal(suite.T(), "failed to resolve Property: 'TEST', ensure that property is configured in the application", err.Error())
+	assert.NotNil(t, err, "resolveObject error")
+	assert.Equal(t, "failed to resolve Property: 'TEST', ensure that property is configured in the application", err.Error())
 
 	_, err = resolveObject(map[string]interface{}{
 		"test1": "=$env[TEST]",
 	})
-	assert.NotNil(suite.T(), err, "resolveObject error")
-	assert.Equal(suite.T(), "failed to resolve Environment Variable: 'TEST', ensure that variable is configured", err.Error())
+	assert.NotNil(t, err, "resolveObject error")
+	assert.Equal(t, "failed to resolve Environment Variable: 'TEST', ensure that variable is configured", err.Error())
 } 
 
 func (suite *TriggerTestSuite) TestHandlerHandleMessage() {
@@ -195,12 +188,6 @@ func TestTriggerTestSuite(t *testing.T) {
 	suite.Run(t, new(TriggerTestSuite))
 }
 
-func RunServerWithOptions(opts *server.Options) *server.Server {
-	return natsserver.RunServer(opts)
-}
-
-func RunServerOnPort(port int) *server.Server {
-	opts := natsserver.DefaultTestOptions
-	opts.Port = port
-	return RunServerWithOptions(&opts)
+func RunServerWithOptions() *server.Server {
+	return natsserver.RunServer(&natsserver.DefaultTestOptions)
 }
